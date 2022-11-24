4a10f6558197b82395e953a134084db2236fcfe9
Lorenzo Levantesi <lorenzo2609@hotmail.it>
2022-11-24 14:41:27 +0100

    Restructured code and added tolerance

--- code/hypothesis_testing.py
+++ code/hypothesis_testing.py
@@ -5,6 +5,10 @@ from multiprocessing import Process, Array, Value, Barrier
 
 # execute N run of nevergrad, in each run we have num_workers parallel evaluation of the function, then we take the minim evaluation among num_workers as temp result of nevergrad
 
+num_proc = 0
+num_points = 0
+dim = 0
+
 def init_function(function):
 	json_path = os.path.join(os.path.dirname(__file__),"sfu/functions/functions.json")
 	ev.load_json(os.path.join(os.path.dirname(__file__),"sfu/functions/functions.json"))
@@ -15,11 +19,6 @@ def minimum_f(dim=None):
 
 def evaluate_parallel(inp, multi_proc):
 	return ev.evaluate(inp, multi_proc=multi_proc)
-
-def entropy(samples):
-	sum_samples = sum([math.exp(x) for x in samples])
-	prob_samples = [math.exp(x) / sum_samples for x in samples]
-	return - sum([x * math.log2(x) for x in prob_samples])
 
 class CustomProcess(Process):
 	def __init__(self, arr_inp, var_res, process_idx, barrier_compute, barrier_computed, stop_search, compute):
@@ -50,18 +49,10 @@ class CustomProcess(Process):
 			if self.compute.value:
 				res = evaluate_parallel(self.arr_inp[:], [True, self.process_idx])
 				self.var_res.value = res
+
 			self.barrier_computed.wait()
 
-def hypothesis_testing(delta, epsilon, num_points, num_proc, dim, lower_bound = None, upper_bound = None):
-	N = math.ceil((math.log(delta)/math.log(1-epsilon)))
-	if lower_bound != None and upper_bound != None:
-		param = ng.p.Array(shape=(dim,), lower=lower_bound, upper=upper_bound)
-	else:
-		param = ng.p.Array(shape=(dim,))
-
-	# init nevergrad optimizer
-	optimizer = ng.optimizers.NGOpt(parametrization=param, budget=10**6)
-
+def create_processes(num_proc, dim):
 	# init of the multiprocessing
 	# arr_inp is a list which contains num_proc n-dimensional arrays, each shared array will contain the function input which the process must compute
 	arr_inp = [Array("d", [0 for x in range(dim)]) for i in range(num_proc)]
@@ -83,77 +74,129 @@ def hypothesis_testing(delta, epsilon, num_points, num_proc, dim, lower_bound = 
 	for x in range(num_proc):
 		processes.append(CustomProcess(arr_inp[x], arr_res[x], x, barrier_compute, barrier_computed, stop_search, compute[x]))
 
+	return processes
+
+def compute_points(processes, input_points):
+	global num_proc, num_points, dim
+	barrier_compute = processes[0].barrier_compute
+	barrier_computed = processes[0].barrier_computed
+	computed_points = []
+
+	# execute num_proc evaluations, for num_points // num_proc times
+	for idx in range(num_points//num_proc):
+		for proc in processes:
+			# comunicate input to the process proc
+			for idx_input in range(dim):
+				proc.arr_inp[idx_input] = input_points[idx+proc.process_idx][idx_input]
+
+		# start computation
+		barrier_compute.wait()
+		# wait end of computation
+		while(barrier_computed.n_waiting != num_proc):
+			pass
+		# save results
+		for proc in processes:
+			computed_points.append(proc.var_res.value)
+		# restart
+		barrier_computed.wait()
+
+	# compute rest of the points
+	if (num_points % num_proc != 0):
+		# execute the remaning num of workers
+		num_remaining_points = (num_points % num_proc)
+		for proc in processes[:num_remaining_points]:
+			# comunicate input to process proc
+			for idx_input in range(dim):
+				proc.arr_inp[idx_input] = input_points[(num_points - num_remaining_points) + proc.process_idx][idx_input]
+			
+		# do not assign computation to the remaining processes
+		for proc in processes[-(num_proc-num_remaining_points):]:
+			proc.compute.value = 0
+
+		# start computation
+		barrier_compute.wait()
+		# wait end of computation
+		while(barrier_computed.n_waiting != num_proc):
+			pass
+		# save results
+		for proc in processes[:num_remaining_points]:
+			computed_points.append(proc.var_res.value)
+		# restart
+		barrier_computed.wait()
+
+		# reset compute variables to 1 of processes blocked
+		for proc in processes[-(num_proc-num_remaining_points):]:
+			proc.compute.value = 1
+
+	return computed_points
+
+def obtain_queries(optimizer):
+	global num_points
+	input_points = []
+	for i in range(num_points):
+		query = optimizer.ask()
+		query = list(*query.args)
+		input_points.append(query)
+	return input_points
+
+def update_optimizer(optimizer, input_points, computed_points):
+	global num_points
+	for i in range(num_points):
+		candidate = optimizer.parametrization.spawn_child(new_value = input_points[i])
+		optimizer.tell(candidate, computed_points[i])
+
+
+def hypothesis_testing(delta, epsilon, lower_bound = None, upper_bound = None, tolerance = 1e-6):
+	global num_proc, num_points, dim
+
+	N = math.ceil((math.log(delta)/math.log(1-epsilon)))
+	if lower_bound != None and upper_bound != None:
+		param = ng.p.Array(shape=(dim,), lower=lower_bound, upper=upper_bound)
+	else:
+		param = ng.p.Array(shape=(dim,))
+
+	# init nevergrad optimizer
+	optimizer = ng.optimizers.NGOpt(parametrization=param, budget=10**6)
+
+	processes = create_processes(num_proc, dim)
+	barrier_compute = processes[0].barrier_compute
+	stop_search = processes[0].stop_search
+
 	for proc in processes:
 		proc.start()
 
-	S_prime = math.inf
-	S_values = []
-	num_iterations= 0
+	# init S
+	input_points = obtain_queries(optimizer)
+	computed_points = compute_points(processes, input_points)
+	S_prime = min(computed_points)
+	update_optimizer(optimizer, input_points, computed_points)
+
+	S_values = [S_prime]
+	num_iterations = 0
 	while (not stop_search.value):
 		num_iterations += 1
 		string_run = ''
 		for run in range(N):
 			S = S_prime
-			# list which contains num_points array of function inputs
-			computed_points = []
-			input_points = []
-			for i in range(num_points):
-				query = optimizer.ask()
-				query = list(*query.args)
-				input_points.append(query)
 
-			# execute num_proc evaluations, for num_points // num_proc times
-			for idx in range(0, num_points - (num_points % num_proc), num_proc):
-				for idx_proc in range(num_proc):
-					for idx_input in range(dim):
-						arr_inp[idx_proc][idx_input] = input_points[idx+idx_proc][idx_input]
-				barrier_compute.wait()
-				while(barrier_computed.n_waiting != num_proc):
-					pass
-				for idx_proc in range(num_proc):
-					computed_points.append(arr_res[idx_proc].value)
-				barrier_computed.wait()
-
-			# compute rest of the points
-			if (num_points % num_proc != 0):
-				# execute the remaning num of workers
-				for idx in range(num_points - (num_points % num_proc), num_points):
-					for idx_input in range(dim):
-						arr_inp[num_points-idx-1][idx_input] = input_points[idx][idx_input]
-			
-				# do not assign computation to the remaining processes
-				for idx in range((num_points % num_proc), num_proc):
-					compute[idx].value = 0
-
-				# start computations
-				barrier_compute.wait()
-				# wait until all computations are terminated
-				while(barrier_computed.n_waiting != num_proc):
-					pass
-				# retrieve results
-				for idx in range(num_points - (num_points % num_proc), num_points):
-					computed_points.append(arr_res[num_points-idx-1].value)
-				barrier_computed.wait()
-
-				# reset compute variables to 1
-				for x in range((num_points % num_proc), num_proc):
-					compute[x].value = 1
-
-			# tell results to optimizer
-			for i in range(num_points):
-				candidate = optimizer.parametrization.spawn_child(new_value = input_points[i])
-				optimizer.tell(candidate, computed_points[i])
+			input_points = obtain_queries(optimizer)
+		
+			computed_points = compute_points(processes, input_points)
 
 			# take the minimum of the num_points evaluations
 			result = min(computed_points)
+
 			print(f"Run: {run+1}/{N}\tResult: {result}\tS: {S}")
 			string_run = string_run + f"Run: {run+1}/{N}\tResult: {result}\tS: {S}\n"
 
+			update_optimizer(optimizer, input_points, computed_points)
+
 			# if result smaller than starting S, restart
-			if result < S:
+			if (result + tolerance) < S:
 				S_prime = result
 				S_values.append(S_prime)
 				break
+
 		# if after the loop starting S is equal to S_prime, end algorithm
 		if S == S_prime:
 			stop_search.value = 1
@@ -161,6 +204,7 @@ def hypothesis_testing(delta, epsilon, num_points, num_proc, dim, lower_bound = 
 		with open(f"{os.path.dirname(__file__)}/temp_res/temp.txt", "w") as f:
 			f.write(string_run)
 			f.close()
+
 	# free processes to finish them
 	barrier_compute.wait()
 	for proc in processes:
@@ -169,26 +213,31 @@ def hypothesis_testing(delta, epsilon, num_points, num_proc, dim, lower_bound = 
 	# obtain best value found 
 	recommendation = optimizer.provide_recommendation()
 	print(f"Num iterations: {num_iterations}")
+	print(f"S values: {S_values}")
 	return (list(*recommendation.args), evaluate_parallel(list(*recommendation.args), [False]))
 
 def main(argv):
 	# argv[0] : function name, argv[1] : dimension, argv[2] : # points to evaluate, argv[3] : # parallel processes
+	global num_proc, num_points, dim
+
 	start_time = time.time()
-	# init of the function to evaluate
-	function_name = argv[0]
+	
 	# init the module which compute the function and the infos about it
+	function_name = argv[0]
 	init_function(function_name)
 	dim = int(argv[1])
 	min_f = minimum_f(dim)
-	
+	num_points = int(argv[2])
+	num_proc = int(argv[3])
 	range_f = ev.input_domain(dim)
 	bounds = [[],[]]
 	for x in range(dim):
 		bounds[0].append(range_f[x][0])
 		bounds[1].append(range_f[x][1])
+
 	print(f"minimum_f: {min_f}")
 	print(f"Lower bounds: {bounds[0]}\tUpper bounds: {bounds[1]}")
-	print(f"RESULT: {hypothesis_testing(0.001, 0.001, int(argv[2]), int(argv[3]), dim, bounds[0], bounds[1])}")
+	print(f"RESULT: {hypothesis_testing(0.001, 0.001, bounds[0], bounds[1])}")
 	print(f"Time: {time.time() - start_time}")
 
 
