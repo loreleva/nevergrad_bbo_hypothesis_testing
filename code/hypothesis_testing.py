3c86b047a3235f822fa4ad20979c231ec7e999c6
Lorenzo Levantesi <lorenzo2609@hotmail.it>
2022-12-01 19:16:04 +0100

    Tracking memory

--- code/hypothesis_testing.py
+++ code/hypothesis_testing.py
@@ -2,17 +2,21 @@ import nevergrad as ng
 import sfu.evaluation_code.functions as ev
 import time, sys, os, math
 from multiprocessing import Process, Queue
+from memory_profiler import memory_usage
 
 # execute N run of nevergrad, in each run we have num_workers parallel evaluation of the function, then we take the minim evaluation among num_workers as temp result of nevergrad
 
 num_proc = 0
 num_points = 0
 dim = 0
+parameters = None
 q_inp = None
 q_res = None
 lower_bound = None
 upper_bound = None
 range_stopping_criteria = None
+path_log_file = None
+log_string = ""
 
 def init_function(function):
 	json_path = os.path.join(os.path.dirname(__file__),"sfu/functions/functions.json")
@@ -25,7 +29,10 @@ def minimum_f(dim=None):
 def proc_function(q_inp, q_res):
 	inp = q_inp.get()
 	while (inp != None):
+		start_time = time.process_time()
+		mem_usage = memory_usage(os.getpid(), interval=.1)
 		res = run_nevergrad()
+		res = ((time.process_time() - start_time, max(mem_usage)), res)
 		q_res.put(res)
 		inp = q_inp.get()
 
@@ -71,13 +78,14 @@ def update_optimizer(optimizer, input_points, computed_points):
 		optimizer.tell(candidate, computed_points[i])
 
 def compute_points(input_points):
+	global parameters
 	results = []
 	for inp in input_points:
-		results.append(ev.evaluate(inp))
+		results.append(ev.evaluate(inp, param=parameters))
 	return results
 
 def run_nevergrad():
-	global lower_bound, upper_bound, range_stopping_criteria
+	global lower_bound, upper_bound, range_stopping_criteria, dim, log_string
 
 	if lower_bound != None and upper_bound != None:
 		param = ng.p.Array(shape=(dim,), lower=lower_bound, upper=upper_bound)
@@ -103,29 +111,36 @@ def run_nevergrad():
 		#print(f"RESULT : {results}")
 		update_optimizer(optimizer, input_points, computed_points)
 	recommendation = optimizer.provide_recommendation()
-	return (list(*recommendation.args), ev.evaluate(list(*recommendation.args)))
+	return (optimizer.num_ask, list(*recommendation.args), ev.evaluate(list(*recommendation.args)))
 
 
 def write_log_file(path, string_res):
 	# write temp results
-	with open(f"{os.path.dirname(__file__)}" + path, "w") as f:
+	with open(path, "w") as f:
 		f.write(string_res)
 		f.close()
 
 
 def hypothesis_testing(delta, epsilon, tolerance = 1e-6):
-	global q_inp, q_res
+	global q_inp, q_res, dim, parameters, log_string, num_proc
 	N = math.ceil((math.log(delta)/math.log(1-epsilon)))
 	print(f"N: {N}")
-	start_exec_time = time.time()
-
+	total_process_time = 0
+	max_ram_usage = 0
 	# init S
-	path = "/temp_res/temp.txt"
 	q_inp.put(1)
 	res = q_res.get()
+	total_process_time += res[0][0]
+	if max_ram_usage < res[0][1]:
+		max_ram_usage = res[0][1]
+	temp_string = f"FIRST RESULT: {res[1][2]}\tNUM OF ASKS: {res[1][0]}\tTIME: {res[0][0]}\tMAX RAM USAGE: {res[0][1]} MB"
+	print(temp_string)
+	log_string = log_string + '\n' + temp_string
+	res = res[1]
 	S_values = [res]
-	S_prime = res[1]
+	S_prime = res[2]
 	num_iterations = 0
+	num_iter_internal = 0
 	write_string = ""
 	while (1):
 		S = S_prime
@@ -137,16 +152,21 @@ def hypothesis_testing(delta, epsilon, tolerance = 1e-6):
 
 		counter_samples = 0
 		while(counter_samples < N):
+			num_iter_internal += 1
 			counter_samples += 1
-			start_time = time.time()
 			res = q_res.get()
-			temp_string = f"RESULT: {res[1]}\tS: {S}\tITERATION: {counter_samples}/{N}\tTIME: {time.time() - start_time}"
+			total_process_time += res[0][0]
+			if max_ram_usage < res[0][1]:
+				max_ram_usage = res[0][1]
+			temp_string = f"RESULT: {res[1][2]}\tS: {S}\tNUM OF ASKS: {res[1][0]}\tITERATION: {counter_samples}/{N}\tTime: {res[0][0]}\tMAX RAM USAGE: {res[0][1]} MB"
 			print(temp_string)
-			write_string = write_string + '\n' + temp_string
-			write_log_file(path, write_string)
+			log_string = log_string + '\n' + temp_string
+			res = res[1]
+			
+
 			# if result smaller than starting S, restart
-			if (res[1] + tolerance) < S:
-				S_prime = res[1]
+			if (res[2] + tolerance) < S:
+				S_prime = res[2]
 				S_values.append(res)
 				write_string = ""
 				break
@@ -156,43 +176,54 @@ def hypothesis_testing(delta, epsilon, tolerance = 1e-6):
 			break
 		
 
-	temp_string = f"Num iterations: {num_iterations}\nS values: {S_values}"
-	write_string = write_string + "\n" + temp_string
+	temp_string = f"Num external iterations: {num_iterations}\nNum internal iterations: {num_iter_internal}\nRESULT: {S_values[-1]}\nTime: {total_process_time}\nMEAN TIME PER PROCESS: {total_process_time/num_proc}\nMAX RAM USAGE: {max_ram_usage} MB\nS values: {S_values}"
+	log_string = log_string + "\n" + temp_string
 	print(temp_string)
-	write_log_file(path, write_string)
-	return S_values[-1]
+	return (total_process_time, S_values[-1])
 
 def main(argv):
-	# argv[0] : function name, argv[1] : dimension, argv[2] : # points to evaluate, argv[3] : # parallel processes
-	global num_proc, num_points, dim, lower_bound, upper_bound, range_stopping_criteria
+	# argv[0] : function name, argv[1] : # points to evaluate, argv[2] : # parallel processes
+	global num_proc, num_points, dim, parameters, lower_bound, upper_bound, range_stopping_criteria, path_log_file, log_string
 
-	start_time = time.time()
-	
+	processes_time = 0
+	path_dir_res = os.path.join(os.path.dirname(__file__),"log_results")
+	if not os.path.exists(path_dir_res):
+		os.mkdir(path_dir_res)
 	# init the module which compute the function and the infos about it
 	function_name = argv[0]
 	init_function(function_name)
-	dim = int(argv[1])
-	min_f = minimum_f(dim)
-	num_points = int(argv[2])
-	range_f = ev.input_domain(dim)
-	bounds = [[],[]]
-	for x in range(dim):
-		bounds[0].append(range_f[x][0])
-		bounds[1].append(range_f[x][1])
-	lower_bound = None#bounds[0]
-	upper_bound = None#bounds[1]
+	param_a = [20, 50]
+	param_b = [0.2, 0.8]
+	param_c = [2*math.pi, 7*math.pi]
+	dimensions = [2] + [10**(x+1) for x in range(6)]
+	num_points = int(argv[1])
+	num_proc = int(argv[2])
 	range_stopping_criteria = 20
+	delta = 0.001
+	epsilon = 0.001
+	for temp_dim in dimensions:
+		min_f = minimum_f(temp_dim)
+		path_dir_res_dim = os.path.join(path_dir_res, f"dim_{temp_dim}")
+		if not os.path.exists(path_dir_res_dim):
+			os.mkdir(path_dir_res_dim)
+		for par_a in param_a:
+			for par_b in param_b:
+				for par_c in param_c:
+					dim = temp_dim
+					parameters = {"a" : par_a, "b" : par_b, "c" : par_c}
+					log_string = f"TESTING FUNCTION DIM: {temp_dim} PARAM_A: {par_a} PARAM_B: {par_b} PARAM_C {par_c} OPT_POINT: {min_f}\n"
+					path_log_file = os.path.join(path_dir_res_dim, f"log_dimension-{dim}_param_a-{par_a}_param_b-{par_b}_param_c-{str(par_c).replace('.','-')}.txt")
+					print(log_string)
+					processes = init_processes()
+					hypo_testing_res = hypothesis_testing(delta, epsilon)
+					temp_string = f"ERROR: {abs(min_f - hypo_testing_res[1][2])}"
+					print(temp_string + "\n\n\n")
+					log_string = log_string + "\n" + temp_string
+					processes_time += hypo_testing_res[0]
+					kill_processes(processes)
+					write_log_file(path_log_file, log_string)
 
-	num_proc = int(argv[3])
-	processes = init_processes()
-
-	print(f"minimum_f: {min_f}")
-	print(f"Lower bounds: {bounds[0]}\tUpper bounds: {bounds[1]}")
-	print(f"RESULT hypothesis testing: {hypothesis_testing(0.001, 0.001)}")
-	kill_processes(processes)
-	print(f"Time: {time.time() - start_time}")
-
-
+	print(f"TOTAL TIME: {processes_time}\tMEAN PER PROCESS TIME: {processes_time/num_proc}")
 	
 
 if __name__ == "__main__":
