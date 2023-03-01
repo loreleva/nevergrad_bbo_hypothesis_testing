b51cc4e2d9759fb6a9493783a65b6fceaa6d7e11
Lorenzo Levantesi <lorenzo2609@hotmail.it>
2023-03-01 23:06:50 +0100

    typo

--- code/hypothesis_testing.py
+++ code/hypothesis_testing.py
@@ -39,7 +39,7 @@ def multiproc_function(q_inp, q_res, function_obj, num_points):
 
 def hypothesis_testing(input_opt, opt, delta, epsilon, path_dir_log_file, num_proc, q_inp, q_res, tolerance = 1e-6, correct_thr = 1e-6):
 	# init N
-	N = 5#math.ceil((math.log(delta)/math.log(1-epsilon)))
+	N = math.ceil((math.log(delta)/math.log(1-epsilon)))
 
 	# track execution time of algorithm
 	start_time = time.time()
